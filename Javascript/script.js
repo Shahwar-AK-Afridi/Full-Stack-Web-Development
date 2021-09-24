1ff2f07562938ab001dac4d38324bbdf73177666
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2021-09-24 19:48:02 +0500

    Learning JavaScript

---
+++ Javascript/script.js
@@ -0,0 +1,51 @@
+alert("hello world");
+document.write("Nigga is here");
+var age = 45;
+var nation = "Pakistani";
+var feepaid = true;
+
+alert(age);
+alert(nation);
+
+console.log(age);
+console.log(feepaid);
+
+age = 21;
+console.log(age);
+
+//Variable for string
+var name = "Shahwar Afridi"
+console.log(name);
+name = "Arham Afridi";
+console.log(name);
+var message = "What is your Fathser's Name";
+console.log(message);
+
+//Variable for number
+var weight = 80.5;
+console.log(weight);
+var newweight = weight + 20;
+console.log(newweight);
+
+//Variable for boolean
+var isfeepaid = true;
+console.log(isfeepaid);
+
+//Undefined vs Null
+var learn;
+console.log(learn); //console par undefined lekha ayega
+learn = "Undefined vs Null";
+console.log(learn);
+learn = undefined;
+console.log(learn);
+
+var yoyo = null;
+console.log(yoyo);
+
+//Dynamic Datatypes
+var a = 35;
+console.log(a);
+a = "apple";
+console.log(a);
+a = true;
+console.log(a);
---
+++ Javascript/script2.js
@@ -0,0 +1,15 @@
+console.log("Hello Nigga");
+console.log("Hello world")
+console.log("34")
+var age = 45;
+var nation = "Pakistani";
+var feepaid = true;
+
+console.log(age);
+console.log(feepaid);
+
+//Variable for number
+var weight = 80.5;
+console.log(weight);
+var newweight = weight + 20;
+console.log(newweight);
